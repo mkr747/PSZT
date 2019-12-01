6c89ebeea0eefc6473c3ccd52e15ab9ce2c61f47
mkr747 <marcinkretkowski747@gmail.com>
2019-12-01 17:01:27 +0100

    Change of member

--- Member.py
+++ Member.py
@@ -1,12 +1,16 @@
+import Currency from Currency
+
 class Memeber:
     _id = 0
     Change = 0
 
-    def __init__(self):
+    def __init__(self, currency):
         Memeber._id = Memeber._id + 1
         self.id = Memeber._id
         self.fitnessValue = -1
-        self.change = Change
-    
+        self.change = Member.Change
+        for coin in currency.coins:
+            self.coins[coin] = 0
+
     def setChange(value):
         Memeber.Change = value
